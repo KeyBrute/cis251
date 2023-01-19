8827f93f57e7cd4ab50b3270770c58531c7baa3c
Miguel <mickeymadrigal@yahoo.com>
2023-01-19 11:15:18 -0600

    sdf

--- Miguel/lab2/lab2.cpp
+++ Miguel/lab2/lab2.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <bits/stdc++.h>
+
+
 using namespace std;
 
 int main () {
+    // Opening the file
+    ifstream file("datalocations.txt");
+ 
+    vector<string> v;
+    string str;
+ 
+    // Read the next line from File until it reaches the
+    // end.
+    while (file >> str) {
+        // Now keep reading next line
+        // and push it in vector function until end of file
+        v.push_back(str);
+    }
+ 
+    // Printing each new line separately
+    copy(v.begin(), v.end(),
+         ostream_iterator<string>(cout, "\n"));
+ 
+    return 0;
+}
